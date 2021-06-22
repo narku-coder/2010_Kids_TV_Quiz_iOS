e8a371633311a46d5a28922194976a4decd49234
bcm100 <bcm100@txstate.edu>
2021-06-22 12:16:34 -0500

    Initial Commit

---
+++ 2000s TV Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2000s TV Quiz
+//
+//  Created by Bryan Jones on 6/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2000s TV Quiz/_000s_TV_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  _000s_TV_QuizApp.swift
+//  2000s TV Quiz
+//
+//  Created by Bryan Jones on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct _000s_TV_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
